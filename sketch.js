221c9d2452b9cd2c0d94337ce4d2c1ada13751ae
anT1354 <126083411+anT1354@users.noreply.github.com>
2023-02-22 14:56:17 +0100

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,41 @@
+var canvas;
+let image1;
+let graphic;
+
+function setup() {
+  canvas = createCanvas(windowWidth, windowHeight * 10);
+  canvas.position(0, 0);
+  canvas.style('z-index', '-10');
+  image1 = loadImage("56jj1v.jpg");
+  graphic = createGraphics(windowWidth,windowHeight*10)
+  
+  
+  
+
+   
+
+}
+
+function draw() {
+  background(100)
+  clear();
+  push();
+  background(160, 0, 128);
+  fill(160, 0, 128);
+  strokeWeight(5);
+  stroke(255, 255, 0);
+  circle( mouseX, mouseY, 50);
+  pop();
+  
+  
+  image(image1,mouseX - 203, mouseY -62, 250, 250);
+  
+   if (mouseIsPressed) {
+     for(let i=0; i < 2000; i++) {
+      graphic.square(random(0 ,width),random(0 ,height*10), 50)
+      graphic
+    }
+
+  }
+  image(graphic, 0, 0);
+}
